36317c404a4e3c0df44a75682c621e4f37e49ddc
Malvin Gattinger <malvin@w4eg.de>
2019-06-19 14:26:04 +0200

    Setup.hs: s/getCurrentDirectory/libdir; add postConf/preBuild hooks

--- Setup.hs
+++ Setup.hs
@@ -10,6 +10,8 @@ main = defaultMainWithHooks simpleUserHooks
   {
     preConf = makeExtLib
   , confHook = \a f -> confHook simpleUserHooks a f >>= updateExtraLibDirs
+  , postConf = disablePostConfHooks -- seems crucial to not reset extraLibDir
+  , preBuild = updateLibDirs
   , postCopy = copyExtLib
   , postClean = cleanExtLib
   }
@@ -21,30 +23,43 @@ makeExtLib _ flags = do
         ["make", "--directory=ext_lib"]
     return emptyHookedBuildInfo
 
+updateLibDirs :: Args -> BuildFlags -> IO HookedBuildInfo
+updateLibDirs _ _ = do
+    dir <- getCurrentDirectory
+    putStrLn $ "NOTE: preBuild: putting " ++ dir ++ "/ext_lib/lib into extraLibDirs"
+    let extlibDir = dir ++ "/ext_lib/lib"
+        bi = emptyBuildInfo { extraLibDirs = [ extlibDir ] }
+    return (Just bi, [])
+
 updateExtraLibDirs :: LocalBuildInfo -> IO LocalBuildInfo
 updateExtraLibDirs localBuildInfo = do
     let packageDescription = localPkgDescr localBuildInfo
         lib = fromJust $ library packageDescription
         libBuild = libBuildInfo lib
-    dir <- getCurrentDirectory
+        libPref = libdir $ absoluteInstallDirs packageDescription localBuildInfo NoCopyDest
+    putStrLn $ "NOTE: confHook: adding " ++ libPref ++ " to extraLibDirs"
     return localBuildInfo {
         localPkgDescr = packageDescription {
             library = Just $ lib {
                 libBuildInfo = libBuild {
-                    extraLibDirs = (dir ++ "/ext_lib/lib") :
-                        extraLibDirs libBuild
+                    extraLibDirs = libPref : extraLibDirs libBuild
                 }
             }
         }
     }
+
+disablePostConfHooks :: Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+disablePostConfHooks args flags pd lbi = return ()
 
 copyExtLib :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 copyExtLib _ flags pkg_descr lbi = do
     let libPref = libdir . absoluteInstallDirs pkg_descr lbi
                 . fromFlag . copyDest
                 $ flags
-    let verbosity = fromFlag $ copyVerbosity flags
-    rawSystemExit verbosity "cp" ["ext_lib/lib/libext.a", libPref]
+        config = configFlags lbi
+        verb = fromFlag $ configVerbosity config
+    putStrLn $ "NOTE: postcopy: installing ext_lib/lib/libext.a to " ++ libPref ++ "/libext.a"
+    installExecutableFile verb "ext_lib/lib/libext.a" (libPref ++ "/libext.a")
 
 cleanExtLib :: Args -> CleanFlags -> PackageDescription -> () -> IO ()
 cleanExtLib _ flags _ _ =
